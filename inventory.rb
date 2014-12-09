b68cb4ec00d614c5bce311d27821778903d7255a
Josh Cheek <josh.cheek@gmail.com>
2014-12-08 21:46:34 -0700

    Britney's inventory! Rails Kata!

---
+++ inventory.rb
@@ -0,0 +1,8 @@
+inventory = [
+  { :name=>"Rails kata",
+    :description => "Get a feel for the parts of a Rails app and how they fit together by implementing the code that scaffold would write for you"
+  }
+]
+
+require 'pp'
+pp inventory
