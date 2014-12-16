8ab1c8fac5f6c15efa21ae2f00121b7125a21daf
Josh Cheek <josh.cheek@gmail.com>
2014-12-15 19:03:21 -0700

    Add moar inventory! - All the shit I wish people would have told me (about Ruby)

--- inventory.rb
+++ inventory.rb
@@ -1,6 +1,10 @@
 inventory = [
   { :name=>"Rails kata",
     :description => "Get a feel for the parts of a Rails app and how they fit together by implementing the code that scaffold would write for you"
+  },
+  { :name=>"Ruby intro for experienced devs",
+    :description=>
+     "flags / commandline invocations\nglobals\nobject model\nconstant lookup\nreflection\nto_s vs inspect\nputs vs p\nload path\nnaming conventions\nfinding code\nrubygems\nwhich version\nmethod args\noverriding & overwriting\nblocks\ndo/end vs {}\nequality\nprimitives\n  definition\n  common ones\ngarbage collection\nlogic\nexception\npry\ninterpolation\nbundler\n"
   }
 ]
 
